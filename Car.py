caf60f1653709d4c0cad60bcd36985a73d7523e0
Nichi <nicholas.stantoncook@my.jcu.edu.au>
2016-05-17 23:52:11 +1000

    Modified the existing class Car to include and Inheritance class Taxi

--- Car.py
+++ Car.py
@@ -18,6 +18,29 @@ This method will increase the cars odometer while subtracting the corresponding 
 stop if the distance traveled exceeds the amount of fuel in the variable self.fuel
 
 also includes a String method for specific print formatting
+
+
+Inheritance Class: Taxi
+Author: Nicholas Stanton-Cook
+Program Set: Week nine Workshop Tasks, supplied by Faculty
+This Class will Inherit information and methods from the Car class and add the following
+
+new Parameters
+-price_per_kilometer
+-current_fare_distance
+
+Changes to existing Methods
+-Overrides the __str__ method to display the original Car __Str__ method plus the following
+    -price per Km
+    -current fare distance
+
+-Changes the drive() method to include details about the current fare distance
+
+New Methods
+-get_fare()
+    gets the total cost of the current fare
+-start_fare()
+    resets the values of current_fare_distance
 """
 __Author__ = "Original Author: CP1404 Faculty\nModified by: Nicholas Stanton-Cook"
 
@@ -58,3 +81,29 @@ class Car:
         """
 
         return "{}, Fuel={}, Odometer={}".format(self.name, self.fuel, self.odometer)
+
+
+class Taxi(Car):
+    def __init__(self, name, fuel, price_per_km):
+        """ initialise a Taxi instance, based on parent class Car """
+        super().__init__(name, fuel)
+        self.price_per_km = price_per_km
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        """ return a string representation like a car but with current fare distance"""
+        return "{}, ${:.2f}/km, {}km on current fare".format(super().__str__(), self.price_per_km, self.current_fare_distance)
+
+    def get_fare(self):
+        """ get the price for the taxi trip """
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """ begin a new fare """
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        """ drive like parent Car but calculate fare distance as well"""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
