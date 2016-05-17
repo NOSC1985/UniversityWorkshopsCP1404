dee5e091871011d19ba048b568594b1311052719
Nichi <nicholas.stantoncook@my.jcu.edu.au>
2016-05-17 23:05:20 +1000

    Completed the functionality of "meters_to_kilometers.py"

--- Week9/meters_to_kilometers_converter.py
+++ Week9/meters_to_kilometers_converter.py
@@ -11,14 +11,34 @@ class BoxLayoutDemo(App):
         return self.root
 
     def handle_conversion(self):
-        self.root.ids.output_screen.text = "Hello {}".format(self.root.ids.user_input.text)
+        current_user_entered_value = float(self.root.ids.user_input.text)
+        converted_value = current_user_entered_value / 1000
+        if converted_value < 0.01:
+            self.root.ids.output_screen.text = "Less than 0.01km"
+
+        else:
+            self.root.ids.output_screen.text = "Approximately {:.2f}km".format(converted_value)
 
     def handle_up(self):
-        self.root.ids.output_screen.text = ''
-        self.root.ids.user_input.text = ''
+        try:
+            input_string = self.root.ids.user_input.text
+            float_number_value = float(input_string)
+            new_display_value = float_number_value + 0.1
+            self.root.ids.user_input.text = "{:.2f}".format(new_display_value)
+
+        except ValueError:
+            self.root.ids.user_input.text = ''
+            self.root.ids.output_screen.text = "Error, please enter a number."
 
     def handle_down(self):
-        self.root.ids.output_screen.text = ''
-        self.root.ids.user_input.text = ''
+        try:
+            input_string = self.root.ids.user_input.text
+            float_number_value = float(input_string)
+            new_display_value = float_number_value - 0.1
+            self.root.ids.user_input.text = "{:.2f}".format(new_display_value)
+
+        except ValueError:
+            self.root.ids.user_input.text = ''
+            self.root.ids.output_screen.text = "Error, please enter a number."
 
 BoxLayoutDemo().run()
