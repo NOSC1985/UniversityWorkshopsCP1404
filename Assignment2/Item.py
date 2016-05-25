f1816d09e5b565a861a08155771f5c253779882d
Nichi <nicholas.stantoncook@my.jcu.edu.au>
2016-05-26 00:25:43 +1000

    Assignment 2 Items For Hire GUI
    To-Do-List
    -Finish Functionality of hire item button
    -Finish Functionality of return item button
    -Add error checking and error display strings
    -Create "Item" class, and Implement
    -Create "ItemList" class and Implement
    -Create functionality where a button will unselect when it is pressed again
    -Implement an Item and Price tracker when hiring multiple items at once
    -Implement an Item tracker when returning multiple items at once
    
    Recently-Finished (25/5/2016)
    -Save data to file option
    -Fixed bug for saving new item popup buttons not having a callback
    -Added display string to update when Items are saved
    -Added functionality to convert List and Dictionaries between each other for easy usage of both
    -Fixed formatting issues between previous version of Items for Hire and Current GUI model
    -Check for '$' input on adding new items popup data (function now removes the $ from the front of the string)
    -Colour coded the current item buttons according to their current availability red: unnavailable green: available
    -Basic Functionality of hire item button started
    -Basic Functionality of return item button started
    
    Finished-Tasks
    -GUI general layout done
    -Kivy file id's allocated
    -Importing of appropriate modules and functions from Kivy suite and previous items for hire text based program
    -Some basic functionality of buttons tested
    -Displaying current items now allows only one to be selected at a time
        (Fixed a bug where the Clear and Cancel buttons were not functioning after fixing the above problem)
    -Popup parameters finished
    -Imported data and reformatted into an Item Dictionary
    -Add new item data correctly stored and displayed
    -Current selected item displays correctly in status text
    -Referenced the resource materials used for studying for creating this program.
    -Added a save and quit button to the GUI

---
+++ Assignment2/Item.py
@@ -0,0 +1,9 @@
+class Item:
+
+    def __init__(self, name, description, price, availability):
+
+        self.name = name
+        self.description = description
+        self.price = price
+        self.availability = availability
+
